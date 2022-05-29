3a3eb41240f39485ca0da24e2c23f0f5fbcd2295
Josh Man <joshuamangerino@gmail.com>
2022-05-28 19:32:39 -0600

    started to add to javaScript

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,19 @@
-const currentCity =document.getElementById("search-city")
+const searchCity =document.getElementById("search-city");
+const searchBtn = document.getElementById("search-btn");
+const searchHistory = document.getElementById("search-history");
+const weatherConditions = document.getElementById("weather-conditions");
+const currentCity = document.getElementById("currentCity");
+const currentTime = document.getElementById("current-time");
+const currentTemp = document.getElementById("current-temp");
+const currentWind = document.getElementById("current-wind");
+const currentHumidity = document.getElementById("current-humidity");
+const currentUv = document.getElementById("current-uv");
+const fiveDayForecast = document.getElementById("five-day-forecast");
 
-const API_Key ='6aca1949aba8c98489dca983da8d1515' 
+const APIKey ='6aca1949aba8c98489dca983da8d1515'; 
 
+getWeatherData()
 function getWeatherData () {
-
+    
 }
+
