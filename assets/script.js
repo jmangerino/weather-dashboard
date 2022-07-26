c6b3c10754beffa6f937173571a91b2ad76d35cd
Josh Man <joshuamangerino@gmail.com>
2022-07-26 15:54:31 -0600

    issue with data not showing

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,134 @@
-const searchBtn = document.getElementById("search-btn");
-const searchHistory = document.getElementById("search-history");
-const weatherConditions = document.getElementById("weather-conditions");
-const currentTime = document.getElementById("current-time");
-const currentTemp = document.getElementById("current-temp");
-const currentWind = document.getElementById("current-wind");
-const currentHumidity = document.getElementById("current-humidity");
-const currentUv = document.getElementById("current-uv");
-const fiveDayForecast = document.getElementById("five-day-forecast");
-const currentCity = document.getElementById("current-city");
+const APIkey = '202716cb2315d5e3eb3aefeea981f8da';
 
+const searchCity = $('search-city');
+const searchBtn = $('search-btn');
+const history = $('history');
 
-const APIKey ='6aca1949aba8c98489dca983da8d1515'; 
+let currentCity; 
 
-const cityList = [];
-
-
-const getWeather = (event) => {
-    let apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + APIKey;
-    fetch(apiURL)
+getWeather = (data) => {
+    let requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.lat}&lon=${data.lon}&exclude=minutely,hourly,alerts&units=imperial&appid=${APIkey}`
+    fetch(requestUrl)
     .then((response) => {
         return response.json();
+        console.log(data);
     })
-    .then((respone) => {
-        const currentDate = new Date(respone.data.dt * 1000);
-        const day= currentDate.getMonth() + 1;
-        const month = currentDate.getFullYear();
-        currentCity.innerHTML = respone.data.name + " (" + month + "/" + day + "/" + year + ") ";
-        currentTemp.innerHTML = respone.data.temp;
-        currentHumidity.innerHTML = respone.data.main.humidity;
-        currentWind.innerHTML = respone.data.wind.spped;
+    .then((data) => {
+        
+        let  weatherConditions = $('weather-conditons');
+        
+        let cityName = $('<h2>');
+        cityName.text(currentCity);
+        weatherConditions.append(cityName);
 
-        let lat = respone.data.coord.lat;
-        let lon = respone.dat.coord.lon;
-        let UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&cnt=1";
-        fetch(UVQueryURL)
-        .then((respone) => {
-            return respone.json();
-        })
-        .then((respones) => {
-            let currentUv = respone.value;
-        });
+        let cityDate = data.current.dt;
+        cityDate = moment.unix(cityDate).format('MM/DD/YYYY');
+        let cityDateEl = ('<span>');
+        cityDate.text(` (${cityDate}) `);
+        cityName.append(cityDateEl);
+
+        let cityTemp = data.current.temp;
+        let cityTempEl = $('<p>')
+        cityTempEl.text(`Temp : ${cityTemp} `)
+        weatherConditions.append(cityTempEl);
+
+        let cityWind = data.current.wind_speed;
+        let cityWindEl = $('<p>');
+        cityWindEl.text(`Wind: ${cityWind} `);
+        weatherConditions.append(cityWindEl);
+
+        let cityHumidity = data.current.humidity;
+        let cityHumidityEl = $('<p>');
+        cityHumidityEl.text(`Humidity ${cityHumidity} `);
+        weatherConditions.append(cityHumidityEl);
+
+        let cityUV = data.current.uvi;
+        let cityUVEl = $('<p>');
+        cityUVEl.text(`UV: ${cityUV} `);
+        weatherConditions.append(cityUVEl);
+
+        let forecast = $('#forecast');
+
+        for (let i = 1; i <= 5; i++) {
+            let date;
+            let temp;
+            let wind;
+            let humidity;
+
+            date = data.daily[i].dt;
+            date = moment.unix(date).format(MM/DD/YYYY);
+
+            temp = data.daily[i].temp.day;
+            icon = data.daily[i].weather[0].icon;
+            wind = data.daily[i].wind_speed;
+            humidity = data.daily[i].humidity;
+
+            let card = document.createElement('div');
+            card.classList.add('card', 'col-2', 'm-1', 'bg-primary', 'text-white');
+
+            let ardBody = document.createElement('div');
+            cardBody.classList.add('card-body');
+            cardBody.innerHTML = `<h6>${date}</h6>
+                                  <img src= "http://openweathermap.org/img/wn/${icon}.png"> </><br>
+                                   ${temp}°C<br>
+                                   ${wind} KPH <br>
+                                   ${humidity}%`
+            
+            card.appendChild(cardBody);
+            fiveDayForecastEl.append(card);
+        }
     })
+return;
 }
 
+displayHistory = () => {
+    let storedCities = JSON.parse(localStorage.getItem("cities")) || [];
+    history.innerHTML ='';
+    for (i = 0; i < storedCities.length; i++) {
+        history.text = `${storedCities[i].city}`;
+    }
+}
 
+getCoordinates = () => {
+    let requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${APIkey}`;
+    let storedCities = JSON.parse(localStorage.getItem('cities')) || [];
 
+    fetch(requestUrl)
+    .then((response) => {
+        return response.json();
+        console.log(json());
+    })
+    .then((data) => {
+        let cityInfo = {
+            city: currentCity,
+            lon: data.coord.lon,
+            lat: data.coord.lat
+        }
+        console.log(data);
+        storedCities.push(cityInfo);
+        localStorage.setItem("cities", JSON.stringify(storedCities));
+
+        displayHistory();
+
+        return cityInfo;
+        console.log(cityInfo);
+    })
+    .then((data) => {
+        getWeather(data);
+        console.log(getWeather(data));
+    })
+    return;
+}
+
+handleCityFormSubmit = (event) => {
+    event.prevendDefault();
+    currentCity = searchCity.val().trim();
+
+    getCoordinates();
+
+    return;
+}
+
+displayHistory();
+
+searchBtn.on("click", handleCityFormSubmit);
